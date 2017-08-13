7584f0a95c706265fa59862bf0d8ff9a3fbb6867
aimfd <1101697681@qq.com>
2017-08-13 22:30:11 +0800

    添加netty websocket 用例

---
+++ src/main/java/com/randioo/mahjong_public_server/websocket/WebSocketServer.java
@@ -0,0 +1,38 @@
+package com.randioo.mahjong_public_server.websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class WebSocketServer {
+    public void start(int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new WebSocketInitializer()) // (4)
+                    .option(ChannelOption.SO_BACKLOG, 128) // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            System.out.println("WebsocketChatServer 启动了");
+
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // 等待服务器 socket 关闭 。
+            // 在这个例子中，这不会发生，但你可以优雅地关闭你的服务器。
+            f.channel().closeFuture().sync();
+
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+
+            System.out.println("WebsocketChatServer 关闭了");
+        }
+
+    }
+}
